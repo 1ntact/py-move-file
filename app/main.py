e1a965f9d441d8a2fae2c103e76a3bfe5d125144
1ntact <verbaivan9@gmail.com>
2026-05-04 16:26:26 +0200

    Done!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+
+    parts = command.split()
+    source = parts[1]
+    destination = parts[2]
+
+    if destination.endswith("/"):
+        destination = destination + os.path.basename(source)
+    dir_path = os.path.dirname(destination)
+
+    if dir_path:
+        os.makedirs(dir_path, exist_ok=True)
+
+    shutil.move(source, destination)
